94b4131633b809e0ba729af8ea2715d0ee92e1df
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-04 18:44:27 -0300

    Seção About e Footer criadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function onScroll() {
-    
-    if(scrollY > 0) {
+
+    if (scrollY > 0) {
         navigation.classList.add("scroll")
     } else {
         navigation.classList.remove("scroll")
@@ -16,7 +16,20 @@ function closeMenu() {
     document.body.classList.remove("menu-expanded")
 }
 
-
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .card
+#about, 
+#about header, 
+`);
 
 onScroll()
 openMenu()
