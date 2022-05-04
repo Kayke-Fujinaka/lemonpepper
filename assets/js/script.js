081143fc1ff1bebb1cf7dba97a1b55fb92f4d1fa
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-04 19:44:45 -0300

    Arrumando bug no Menu Hamburguer com a class "social-links"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,29 @@
 function onScroll() {
-
     if (scrollY > 0) {
-        navigation.classList.add("scroll")
+      navigation.classList.add('scroll')
     } else {
-        navigation.classList.remove("scroll")
+      navigation.classList.remove('scroll')
     }
-
-}
-
-function openMenu() {
-    document.body.classList.add("menu-expanded")
-}
-
-function closeMenu() {
-    document.body.classList.remove("menu-expanded")
-}
-
-ScrollReveal({
+  }
+  
+  function openMenu() {
+    document.body.classList.add('menu-expanded')
+  }
+  
+  function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+  }
+  
+  ScrollReveal({
     origin: 'top',
     distance: '30px',
     duration: 700,
-}).reveal(`
-#home, 
-#home img, 
-#home .stats, 
-#services,
-#services header,
-#services .card
-#about, 
-#about header, 
-`);
-
-onScroll()
-openMenu()
-closeMenu()
+  }).reveal(`
+    #home, 
+    #home img, 
+    #home .stats, 
+    #services,
+    #services header,
+    #services .card
+    #about, 
+    #about header,`)
